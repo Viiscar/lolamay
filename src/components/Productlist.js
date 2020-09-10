edd431d6c028d67a9c224dec9341b2754afe7626
FLR <fanchleroux@tutanota.com>
2020-09-10 14:16:58 -0400

    creating title component

--- src/components/Productlist.js
+++ src/components/Productlist.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import Product from "./Product";
+import React, {useState} from 'react';
+//import Product from "./Product";
+import Title from './Title';
+import {storeProducts} from '../Data'
 function Productlist() {
+    //state
+    const [product, setProduct] = useState([storeProducts]);
+    
     return (
         <div>
             <h3>
-                <Product />
+                <>
+                    <div className="py-5">
+                        <div className="container">
+                            <Title name="our" title="products"/>
+                            <div className="row">
+
+                            </div>
+                        </div>
+                    </div>
+                </>
+                {/* <Product /> */}
             </h3>
         </div>
     )
