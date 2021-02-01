3cc7be2c984d0b01af90afa730862ccbcd9d9874
FLR <fanchleroux@tutanota.com>
2021-02-01 11:20:14 -0400

    revamping about

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,32 @@
 import React from 'react'
 import styled from 'styled-components';
-import logo from '../img/white-logo.png'
+import plant from '../img/planta.png';
 
 export default function About() {
     return (
         <Wrapper className= "row">
-            <div className="left-block mx-auto col-lg-2 col-sm-1">
-                <img src={logo} alt="logo"></img>
+            <div className="left-block mx-auto col-lg-1"></div>
+            <div className="mx-auto col-lg-9 col-sm-12 padding-top center-block">
+                
+                <p className="first-p">Alluance is a female Puerto Rican owned cosmetic brand devoted to creating fresh, cruelty-free, and handmade lipsticks.</p>
+            
+                <p>All lipsticks and lipbalms are made fresh by hand using
+                natural preservatives and organic ingredients.</p>
+
+                <p>By supporting Alluance's natural beauty
+                products, you will make a statement that you do
+                not want to support dangerous commercial
+                beauty products filled with chemicals that are
+                linked to cancer, hormone disruptions, and other
+                health problems.</p>
+                <img className="d-none d-lg-block" src={plant} alt="plant"></img>
             </div>
-            <div className="mx-auto col-lg-10 col-sm-12">
-                <div className="d-flex align-items-end">
-                    <div className="gradient"><span>A</span></div>
-                    <div className="first-line"><p>lluance is a female Puerto Rican
-                        owned cosmetic brand devoted to creating
-                        fresh, cruelty-free, and handmade lipsticks.</p>
-                    </div>
-                </div>
-                <div className="right-block">
-                    <p>All lipsticks are made fresh by hand using
-                    natural preservatives and organic ingredients.</p>
-
-                    <p>By supporting Alluance's natural beauty
-                    products, you will make a statement that you do
-                    not want to support dangerous commercial
-                    beauty products filled with chemicals that are
-                    linked to cancer, hormone disruptions, and other
-                    health problems.</p>
-
-                </div>
-                <div className="bottom-block">
-                    {/* <img src={logo} alt="cruelty free"></img>
-                    <img src={logo} alt="natural"></img>
-                    <img src={logo} alt="hand crafted"></img> */}
-                    <span className="circles circ-black">CRUELTY FREE</span>
-                    <span className="circles circ-red">100% NATURAL</span>
-                    <span className="circles circ-white">HAND CRAFTED</span>
-                </div>
+            <div className="col-lg-2 d-flex flex-column flex-md-row flex-lg-column align-items-center circles-container">
+                <span className="circles circ-black">CRUELTY FREE</span>
+                <span className="line"></span>
+                <span className="circles circ-red">GOOD for your skin</span>
+                <span className="line"></span>
+                <span className="circles circ-white">HAND CRAFTED</span>
             </div>
         </Wrapper>
     )
@@ -45,51 +36,22 @@ const Wrapper = styled.div`
     font-size: 1.7rem;
     .left-block{
         background-color: var(--mainRed);
-        display: flex;
-        align-items: center;
-        justify-content: center;
     }
-    .right-block{
-        text-align: justify;;
-        margin: 0rem 10rem 1.5rem 15rem;
-        /* transform: translate(0px, 4.2rem); */
+    .center-block{
+        text-align: justify;
+        padding-left: 8rem;
+        padding-right: 8rem;
+        padding-top: 5rem;
     }
-    .bottom-block{
-        display: flex;
-        justify-content: space-around;
-        padding-top: 2rem;
-        padding-bottom: 5rem
+    .first-p{
+        line-height: 3rem;
     }
-    .gradient{
-        height: 12rem;
-        width: 16rem;
-        background-image: linear-gradient(to right, rgba(0,0,0,0.1), rgba(0,0,0,0.2), rgba(0,0,0,0.3), rgba(0,0,0,0.4), rgba(0,0,0,0.5), rgba(0,0,0,0.6), rgba(0,0,0,0.7), rgba(0,0,0,0.8),rgba(0,0,0,0.9), black, black, black, black);
-        position: relative;
-        margin-bottom: 4.8rem;
-        display: block;
-        float: left;
-    }
-    .gradient span{
+    .first-p::first-letter {
+        font-size: 400%;
         color: var(--mainRed);
-        font-size: 8rem;
-        line-height: 0.6;
-        position: absolute;
-        bottom: 0px;
-        right: 0px;
     }
-    .first-line{
-        /* transform: translate(-5rem, 0rem);
-        p::first-line {
-        background-color: yellow;
-        line-height: 0.6;
-        transform: translate(0rem, 0rem);
-        } */
-        
-        margin-bottom: 0.5rem;
-        margin-right: 10rem;
-    }
-    div{
-        padding-left: 0px;
+    .circles-container{
+        padding-top: 5rem;
     }
     .circles{
         height: 9rem;
@@ -103,15 +65,52 @@ const Wrapper = styled.div`
     .circ-black{
         background-color: black;
         color: var(--mainRed);
+        box-shadow: -10px 5px 5px rgba(180, 183, 186, 0.7)
     }
     .circ-red{
         background-color: var(--mainRed);
         color: white;
+        box-shadow: 0px -10px 5px rgba(180, 183, 186, 0.7)
     }
     .circ-white{
         background-color: white;
         color: var(--mainRed);
+        box-shadow: 10px 5px 5px rgba(180, 183, 186, 0.7)
+    }
+    .line{
+        height: 4rem;
+        width:1px;
+        background-color: var(--mainRed);
+    }
+    @media only screen and (max-width: 991px) {
+        .line{
+            height: 1px;
+            width: 4rem;
+            background-color: var(--mainRed);
+        }
+        .circles-container{
+            justify-content: center;
+            padding-top: 3rem;
+            padding-bottom: 6rem;
+        }
+    }
+    @media only screen and (max-width: 767px) {
+        .center-block{
+            padding-left: 4rem;
+            padding-right: 4rem;
+        }
+        .circles-container{
+            padding-top: 1rem;
+        }
+        .line{
+            height: 4rem;
+            width:1px;
+        }
+    }
+    @media only screen and (max-width: 582px) {
+        .center-block{
+            padding-left: 2rem;
+            padding-right: 2rem;
+        }
     }
 `
-
-//englober right block
